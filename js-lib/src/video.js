8a216e25aeca9cdacc438d727cce7769d991c4eb
Antipurity <45312367+Antipurity@users.noreply.github.com>
2021-12-26 18:06:28 +0200

    Targets now work.
    Fixed RGB drawing being 0..1 instead of -1..1.

--- js-lib/src/video.js
+++ js-lib/src/video.js
@@ -3,14 +3,19 @@ export default function init(sn) {
     return A(class Video extends sn.Sensor {
         docs() { return `// TODO:
 
-This sensor's output is composed of 1 or more tiles, which are square images.
+This sensor's output is composed of 1 or more tiles, which are square images.    
+It can target 0 or more points, each shown in 1 or more tiles, and can include multiple zoom levels.
 
 Extra options:
 - \`tileDimension = 8\`: each tile edge's length.
 - \`source = Video.stitchTab()\`: where to fetch image data from. \`MediaStream\` or \`<canvas>\` or \`<video>\` or \`<img>\` or a function to one of these.
 - \`monochrome = false\`: make this \`true\` to only report [luminance](https://en.wikipedia.org/wiki/Relative_luminance) and use 3× less data.
-- \`targets = Video.pointers()\`: what to focus rectangles' centers on. This is a live array of \`{x,y}\` objects, or a function to that. If empty, the whole \`source\` will be resized to fit, and zooming will zoom in on the center instead of zooming out.
+- \`targets = Video.pointers()\`: what to focus rectangles' centers on. This is a live array of \`{x,y}\` objects with 0…1 viewport coordinates, or a function to that, called every frame. If empty, the whole \`source\` will be resized to fit, and zooming will zoom in on the center instead of zooming out; if not, the viewed rect will be centered on the target.
 ` }
+        pause() {
+            this._nextTarget && this._nextTarget.pause()
+            super.pause()
+        }
         resume(opts) {
             if (opts) {
                 const td = opts.tileDimension || 8
@@ -25,10 +30,13 @@ Extra options:
                 this._tiles = 1
                 this.monochrome = !!opts.monochrome
                 this.noFeedback = true // TODO: If `source` includes feedback canvases, then set this to false.
-                this.targets = targ
-                this._target = 0
                 //   `typeof src == 'function' && typeof src.onFeedback == 'function'`? (`onFeedback` accepting the data-elem and the feedback-canvas. TODO: But how to synchronize frames?)
-                // TODO: Other props. (For example, handle tiling and zooming-in by forking ourselves, and making `pause` responsible for the forks too.)
+                this.targets = targ
+                this._targetIndex = opts._targetIndex || 0
+                this._opts = A(A(Object.create(null), opts), { source:src, targets:targ, _targetIndex: this._targetIndex+1 })
+                if (!this._nextTarget)
+                    this._nextTarget = null // Another Video, for multi-target support by forking.
+                // TODO: Other props. (For example, handle tiling and zooming-in, and targeting by forking ourselves, and making `pause` responsible for the forks too.)
                 opts.extraValues = 0
                 opts.onValues = Video.onValues
                 opts.values = this._tiles * td*td * (this.monochrome ? 1 : 3)
@@ -36,7 +44,9 @@ Extra options:
                     const cells = Math.ceil(dataLen / (dataEnd - dataStart))
                     const valuesPerCell = Math.ceil(this.values / cells)
                     const tile = dataStart / valuesPerCell / (this.monochrome ? 1 : 3) | 0
-                    return {x:0, y:0} // TODO: Infer target x/y, and offset the tile.
+                    const targets = this._targets()
+                    const targ = targets[this._targetIndex]
+                    return targ ? {x:targ.x*2-1, y:targ.y*2-1} : {x:0, y:0} // TODO: Offset the tile properly.
                 }
                 const zoomOut = 1 // TODO: Set this.zoomOut to this. And make drawing respect this.
                 opts.name = [
@@ -44,14 +54,13 @@ Extra options:
                     ''+td,
                     this.noFeedback ? 0 : (...args) => xy(...args).x * 2 - 1,
                     this.noFeedback ? 0 : (...args) => xy(...args).y * 2 - 1,
-                    Math.log2(zoomOut) / 5 - 1,
+                    Math.min(Math.log2(zoomOut) / 5 - 1, 1),
                     this.noFeedback ? 1 : -1,
                 ]
             }
             super.resume(opts)
         }
-        // TODO: How exactly to allow `targets`?
-        //   Make `onValues` check whether the next element exists, and pause/resume (and maybe create) `this._nextTarget` (with `opts._target = this._target+1`)?
+
         // TODO: Allow many zoom-out levels.
         //   (If no-target, zoom in on the center instead.)
         // TODO: Allow many tiles.
@@ -65,7 +74,21 @@ Extra options:
             const dataSize = targetShape[targetShape.length-1]
             const cells = data.length / dataSize | 0
             const valuesPerCell = Math.ceil(sensor.values / cells)
-            if (sensor._dataContext2d(data, valuesPerCell))
+
+            // Targeting stuff.
+            const targets = this._targets()
+            const target = targets[this._targetIndex]
+            const nextTarget = targets[this._targetIndex+1]
+            if (nextTarget && !this._nextTarget) // Create.
+                this._opts._targetIndex = this._targetIndex + 1,
+                this._nextTarget = new Video(this._opts)
+            if (nextTarget && this._nextTarget && this._nextTarget.paused) // Resume.
+            this._opts._targetIndex = this._targetIndex + 1,
+                this._nextTarget.resume(this._opts)
+            if (!nextTarget && this._nextTarget) // Destroy.
+                this._nextTarget.pause(), this._nextTarget = null
+
+            if (sensor._dataContext2d(data, valuesPerCell, target))
                 sensor.sendCallback(Video.onFeedback, data)
         }
         static onFeedback(feedback, sensor) {
@@ -74,6 +97,11 @@ Extra options:
             //   …Simply upscale from feedback, exactly reversing `_dataContext2d`?
         }
 
+        _targets() {
+            const targets = typeof this.targets == 'function' ? this.targets() : this.targets
+            sn._assert(Array.isArray(targets), "Bad targets")
+            return targets
+        }
         static _sourceToDrawable(source) { // .drawImage and .texImage2D can use the result.
             if (typeof source == 'function') source = source()
             if (source instanceof Promise) {
@@ -95,7 +123,7 @@ Extra options:
             const el = m.get(source)
             return el
         }
-        _dataContext2d(data, valuesPerCell) { // Fills `data`.
+        _dataContext2d(data, valuesPerCell, target) { // Fills `data`.
             let frame = Video._sourceToDrawable(this.source)
             if (frame instanceof Promise)
                 return console.error(frame.error), this.pause(), false
@@ -112,10 +140,19 @@ Extra options:
             this._canvas.width = td, this._canvas.height = tiles * td
             // Draw each tile and get its ImageData, and put that into `data`.
             for (let i = 0; i < tiles; ++i) {
-                this._ctx2d.drawImage(frame,
-                    0, 0, width, height,
-                    0, 0, td, td,
-                )
+                if (!target) // Fullscreen.
+                    this._ctx2d.drawImage(frame,
+                        0, 0, width, height,
+                        0, 0, td, td,
+                    )
+                else { // Around a mouse.
+                    const x = (target.x * width - td/2) | 0
+                    const y = (target.y * height - td/2) | 0
+                    this._ctx2d.drawImage(frame,
+                        x, y, td, td,
+                        0, 0, td, td,
+                    )
+                }
             }
             // Actually draw the data.
             const monochrome = this.monochrome
@@ -126,9 +163,9 @@ Extra options:
                     const G = imageData[4 * (td*td*i + j) + 1] / 255
                     const B = imageData[4 * (td*td*i + j) + 2] / 255
                     if (!monochrome) { // Each tile is 3 successive R/G/B cells.
-                        data[(i*3 + 0) * valuesPerCell + j] = R
-                        data[(i*3 + 1) * valuesPerCell + j] = G
-                        data[(i*3 + 2) * valuesPerCell + j] = B
+                        data[(i*3 + 0) * valuesPerCell + j] = R * 2 - 1
+                        data[(i*3 + 1) * valuesPerCell + j] = G * 2 - 1
+                        data[(i*3 + 2) * valuesPerCell + j] = B * 2 - 1
                     } else // https://en.wikipedia.org/wiki/Relative_luminance
                         data[i * valuesPerCell + j] = (0.2126*R + 0.7152*G + 0.0722*B) * 2 - 1
                 }
@@ -141,12 +178,12 @@ Extra options:
             const inds = new Map
             const passive = {passive:true}
             let attached = false, lastRequest = performance.now()
-            let id = setInterval(() => {
-                // Auto-detach when not needed.
+            let id = setInterval(autodetach, 10000)
+            return attachEvents
+            function autodetach() {
                 if (performance.now() - lastRequest > 15000)
                     detachEvents(), clearInterval(id), id = null
-            }, 10000)
-            return attachEvents
+            }
             function onpointerdown(evt) { // Add/update.
                 if (evt.pointerType === 'touch') return
                 if (evt.touches) return Array.from(evt.touches).forEach(onpointerdown)
@@ -174,7 +211,6 @@ Extra options:
                 return inds.get(id)
             }
             function attachEvents() {
-                if (id == null) return null
                 lastRequest = performance.now()
                 if (attached) return ps
                 addEventListener('touchstart', onpointerdown, passive)
@@ -190,6 +226,7 @@ Extra options:
                     addEventListener('pointermove', onpointerdown, passive)
                     addEventListener('pointerup', onpointerup, passive)
                 }
+                if (id == null) setInterval(autodetach, 10000)
                 attached = true
                 return ps
             }
@@ -220,20 +257,24 @@ The result is usable as the \`targets\` option for \`Video\`.`,
         //   TODO: Request stream from extension if possible, through DOM events. Else:
         //   TODO: document.querySelectorAll('canvas, video, img')
         //   TODO: Clear the canvas, and draw each thing onto it.
-        requestDisplay: A(function() { // With the user's permission, gets a screen/window/tab contents.
+        requestDisplay: A(function(width) { // With the user's permission, gets a screen/window/tab contents. // TODO: Maybe, also accept options, and by default, request 512-width videos or similar?
             // Note that in Firefox, the user has to have clicked somewhere on the page first.
-            const p = navigator.mediaDevices.getDisplayMedia({ audio:true, video:true }).then(s => {
+            const opts = { audio:true, video:width ? { max:{width} } : true }
+            const p = navigator.mediaDevices.getDisplayMedia(opts).then(s => {
                 return p.result = s
             }).catch(e => p.error = e)
+            // TODO: Is it possible to adjust frameRate and/or max width on-the-fly, based on how often these are called?
             return function() { return p }
         }, {
             docs:`[Requests a screen/window/tab stream.](https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getDisplayMedia)
 
 The result is usable as the \`source\` option for \`Video\`.`,
         }),
-        requestCamera: A(function() { // With the user's permission, gets a screen/window/tab contents.
+
+        requestCamera: A(function(width) { // With the user's permission, gets a screen/window/tab contents.
             // Note that in Firefox, the user has to have clicked somewhere on the page first.
-            const p = navigator.mediaDevices.getUserMedia({ audio:true, video:true }).then(s => {
+            const opts = { audio:true, video:width ? { max:{width} } : true }
+            const p = navigator.mediaDevices.getUserMedia(opts).then(s => {
                 return p.result = s
             }).catch(e => p.error = e)
             return function() { return p }
