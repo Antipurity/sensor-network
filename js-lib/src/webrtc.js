65a8c81d3e3e54a412ffb91524edeb787bf03b9b
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-01-06 00:14:04 +0200

    stuff

--- js-lib/src/webrtc.js
+++ js-lib/src/webrtc.js
@@ -12,6 +12,7 @@ Options:
             //   TODO: How to listen to incoming connections, preparing to send data for each one?
             //   TODO: On connection's data, remember it.
             //     ...What should we do on dropped, and on out-of-order messages?...
+            //     TODO: this.peer.ondatachannel = evt => evt.channel??? TODO: What exactly do we do with the channel?
             if (opts) {
                 // TODO:
             }
@@ -30,32 +31,29 @@ Options:
 ` }
         resume(opts) {
             if (opts) {
+                this.getPeer()
                 // TODO:
-                // TODO: Also, initiate a negotiation here, right? …But what if it's already done, and we only need to update our settings…
-                //   TODO: this.peer.createOffer().then(offer => this.peer.setLocalDescription(offer)).then(() => sendToSensor(this.peer.localDescription)).catch(err => ???)
-                //     TODO: This is `onnegotiationneeded()`, right?
-                //   TODO: ...Should this be done only once?... Never resetting the list of connections?
-                //     But what if Handler options contain how to establish the connection?
             }
             return super.resume(opts)
         }
-        // TODO: What else does this do? How do we actually handle data and turn it into feedback, again?
-        //   onValues(then, {data, error, noData, noFeedback, cellShape, partSize}, feedback)
+        getPeer() {
+            // Connects to another sensor network through WebRTC.
+            if (this.peer) return
+            // TODO: Also, initiate a negotiation here, right? …But what if it's already done, and we only need to update our settings…
+            //   TODO: this.peer = new RTCPeerConnection({iceServers:['stun.l.google.com:19302', 'stunserver.org:3478']})
+            //     (The list should be an option. For example: https://gist.github.com/mondain/b0ec1cf5f60ae726202e)
+            //   TODO: this.dataChannel = this.peer.createDataChannel('sn-internet', {
+            //     ordered: false,
+            //     maxRetransmits: 0,
+            //   })
+            //   TODO: this.peer.onicecandidate = evt => evt.candidate && SIGNAL(evt.candidate) TODO: How to signal, exactly?
+            //   TODO: this.peer.onnegotiationneeded = this.peer.createOffer().then(offer => this.peer.setLocalDescription(offer)).then(() => sendToSensor(this.peer.localDescription)).catch(console.error)
+            //   TODO: this.peer.onconnectionstatechange = this.peer.connectionState !== 'connected' && ??? TODO: What do we do, exactly? this.peer.close(), this.peer=null, so that `onValues` can attempt to re-open it?
+        }
+        // TODO: onValues(then, {data, error, noData, noFeedback, cellShape, partSize}, feedback)
     }
-    // Unreliable and unordered:
-    // RTCPeerConnection.createDataChannel("label", {
-    //     ordered: false,
-    //     maxRetransmits: 0
-    // })
-    // TODO: How to get an RTCPeerConnection?
-    //   TODO: ...Why do we need an ICE server if we need a separate signaling channel anyway?...
-    // TODO: Call .createDataChannel in Handler and listen to ondatachannel(evt:{channel}) in Sensor.
-    //   ...On handler's `resume`? Does the sensor just accept all channels always? I think so?…
-    // TODO: How to negotiate an ICE connection?
-    //   TODO: What channels do we negotiate over?
-
-    // TODO: …Want to create a demo that can connect, right?
-    //   …We need a signaling server though…
+    // TODO: What to use as signaling? Need options, which default to "the user will manually copy this".
+    //   TODO: But also have a WebSocket option. (Once we have a Rust server to test it on, I mean.)
     return {
         sensor: InternetSensor,
         handler: InternetHandler,
