bcf58c1ce9cf90a2b4d1a6e2596e2f41fd89d5c9
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-15 18:13:33 +0200

    py beginning

--- py/sensornet.py
+++ py/sensornet.py
@@ -14,18 +14,43 @@ class Handler:
         self._error = []
         self._no_data = []
         self._no_feedback = []
+        self._prev_fb = [] # [prev_feedback, …, […, (on_feedback, start_cell, end_cell), …], …]
+        self._next_fb = [] # […, (on_feedback, start_cell, end_cell), …]
+        self.sensors = [] # Called by `.handle(…)`.
         self.cell_shape = ()
         self.part_size = 0
         self.cell_size = 0
-        # TODO: Don't we also want `on_feedback`? For which we want start & end cell-indices…
         if cell_shape is not None or part_size is not None:
             self.shape(cell_shape, part_size)
-    # TODO: shape(self, cell_shape, part_size) that discards accumulated data (calling its on_feedback with None as feedback) and sets cell_shape and part_size.
+    def shape(self, cell_shape, part_size):
+        _shape_ok(cell_shape, part_size)
+        self.discard()
+        self.cell_shape = cell_shape
+        self.part_size = part_size
+        self.cell_size = sum(cell_shape)
     # TODO: send(self, name=None, data, error=None, on_feedback=None)  (`sn.handle` concatenates these) (NumPy data tensors get flattened) (if `data` is a plain number, that's how many no-data cells are being requested) (`on_feedback(feedback, sn)`, called in-order so that re-using a function doesn't break things)
     # TODO: `handle(self, cell_shape, prev_feedback) → (data, error, no_data, no_feedback)` (if `prev_feedback` is a function, it'll be called with no args on each subsequent handling until it returns a non-`None` NumPy tensor; else it should be a NumPy tensor or `None`)
-    #   TODO: Call each of self.sensors.
+    #   TODO: Call each of self.sensors with `sn`.
     #   TODO: Concat all the collected data (& error).
-    # TODO: `sensors: [fn(sn)]`, where each function can call `send`.
+    #   TODO: Empty all our memory of data.
+    #     TODO: Push self._next_fb to self._prev_fb.
+    #     TODO: While len(self._prev_fb) and its first item's first item is either a NumPy array or returns non-None, deconstruct the feedback and call all the `on_feedback` funcs.
+    #       TODO: ...Don't we want to do the same at `handle`'s start, until it can no longer be done?...
+    #   TODO: Return it.
+    def discard(self):
+        """Clears all scheduled-to-be-sent data."""
+        for on_feedback, start_cell, end_cell in self._next_fb:
+            try:
+                on_feedback(None, self)
+            except KeyboardInterrupt:
+                raise
+            except Exception as err:
+                print(err)
+        self._data.clear()
+        self._error.clear()
+        self._no_data.clear()
+        self._no_feedback.clear()
+        self._next_fb.clear()
 
 
 
@@ -66,7 +91,8 @@ def _shape_ok(cell_shape, part_size: int):
     assert len(cell_shape) == 4
     assert all(s % part_size == 0 for s in cell_shape[:-1])
 def _str_to_floats(string: str):
-    return np.frombuffer(string.encode('utf-8'), dtype=np.uint8).astype(np.float32)/255.*2. - 1.
+    hash = hashlib.md5(string.encode('utf-8')).digest()
+    return np.frombuffer(hash, dtype=np.uint8).astype(np.float32)/255.*2. - 1.
 # TODO: Do we want fractal un/filling too?
 
 
