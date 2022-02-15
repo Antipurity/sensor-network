87add5672dd035117b8894f1f3ea2039c2be038e
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-15 20:05:01 +0200

    py handle

--- py/sensornet.py
+++ py/sensornet.py
@@ -7,6 +7,8 @@ class Handler:
     """
     TODO:
 
+    To gather data automatically, do `handler.sensors.append(lambda handler: ...)`.
+
     If needed, read `.cell_shape` or `.part_size` or `.cell_size` wherever the object is available.
     """
     def __init__(self, cell_shape=None, part_size=None):
@@ -15,8 +17,8 @@ class Handler:
         self._error = []
         self._no_data = []
         self._no_feedback = []
-        self._prev_fb = [] # [prev_feedback, …, […, (on_feedback, start_cell, end_cell), …], …]
-        self._next_fb = [] # […, (on_feedback, start_cell, end_cell), …]
+        self._prev_fb = [] # [prev_feedback, …, _next_fb, …]
+        self._next_fb = [] # […, (on_feedback, shape, start_cell, end_cell, namer), …]
         self.sensors = [] # Called by `.handle(…)`.
         self.cell_shape = ()
         self.part_size = 0
@@ -32,6 +34,8 @@ class Handler:
     def send(self, name=None, data=None, error=None, on_feedback=None):
         """
         TODO:
+        TODO: on_feedback(feedback, cell_shape, part_size); `feedback` might be `None`.
+        TODO: data is None or a number (how many no-data cells to request, as an action) or a NumPy array (flat if `name`, else 2D)
         """
         if name is None and on_feedback is None: return
         assert name is None or isinstance(name, tuple)
@@ -48,8 +52,10 @@ class Handler:
         if isinstance(name, tuple) or isinstance(name, list):
             name = Namer(name, self.cell_shape, self.part_size)
         if isinstance(name, Namer):
-            data = name.name(data, False)
-            error = name.name(error, True)
+            assert name.cell_shape == self.cell_shape
+            assert name.part_size == self.part_size
+            data = name.name(data, None)
+            error = name.name(error, -1.)
         else:
             assert name is None
         assert len(data.shape) == 2
@@ -57,20 +63,47 @@ class Handler:
         assert error is None or data.shape == error.shape
         # Send.
         cells = data.shape[0]
+        shape = (cells,)
         self._data.append(data)
         self._error.append(error)
-        self._no_data.append(no_data)
-        self._no_feedback.append(no_feedback)
-        self._prev_fb.append((on_feedback, self._cell, self._cell + cells))
+        self._no_data.append(np.tile(no_data, shape))
+        self._no_feedback.append(np.tile(no_feedback, shape))
+        self._next_fb.append((on_feedback, data.shape, self._cell, self._cell + cells, name))
         self._cell += cells
-    # TODO: `handle(self, cell_shape, prev_feedback) → (data, error, no_data, no_feedback)` (if `prev_feedback` is a function, it'll be called with no args on each subsequent handling until it returns a non-`None` NumPy tensor; else it should be a NumPy tensor or `None`)
-    #   TODO: Call each of self.sensors with `sn`.
-    #   TODO: Concat all the collected data (& error).
-    #   TODO: Empty all our memory of data.
-    #     TODO: Push self._next_fb to self._prev_fb.
-    #     TODO: While len(self._prev_fb) and its first item's first item is either a NumPy array or returns non-None, deconstruct the feedback and call all the `on_feedback(feedback, sn)` funcs.
-    #       TODO: ...Don't we want to do the same at `handle`'s start, until it can no longer be done?...
-    #   TODO: Return it.
+    def handle(self, prev_feedback=None):
+        """
+        TODO:
+        TODO: returns (data, error, no_data, no_feedback); describe each of these.
+        TODO: if `prev_feedback` is a function, it'll be called with no args on each subsequent handling until it returns a non-`None` NumPy tensor; else it should be a NumPy tensor or `None`
+        """
+        assert prev_feedback is None or callable(prev_feedback)
+        # Collect sensor data.
+        for s in self.sensors: s(self)
+        if not len(self._data): return
+        # Gather data.
+        data = np.concatenate(self._data, 0)
+        error = np.concatenate([e or -np.ones((0, self.cell_size)) for e in self._error], 0) if any(self._error) else None
+        no_data = np.concatenate(self._no_data, 0)
+        no_feedback = np.concatenate(self._no_feedback, 0)
+        # Forget this step's data, and report feedback.
+        if prev_feedback is not None:
+            self._prev_fb.append((prev_feedback, self._next_fb, self.cell_shape, self.part_size))
+        else:
+            for on_feedback, expected_shape, start_cell, end_cell, namer in self._next_fb:
+                on_feedback(None, self.cell_shape, self.part_size)
+        self.discard()
+        while len(self._prev_fb):
+            feedback, callbacks, cell_shape, part_size = self._prev_fb[0]
+            if callable(feedback): feedback = feedback()
+            if feedback is None: break
+            assert isinstance(feedback, np.ndarray)
+            self._prev_fb.pop(0)
+            for on_feedback, expected_shape, start_cell, end_cell, namer in callbacks:
+                fb = feedback[start_cell:end_cell, :]
+                assert fb.shape == expected_shape
+                fb = namer.unname(fb)
+                on_feedback(fb, cell_shape, part_size)
+        return (data, error, no_data, no_feedback)
     def discard(self):
         """Clears all scheduled-to-be-sent data."""
         for on_feedback, start_cell, end_cell in self._next_fb:
@@ -113,7 +146,7 @@ class Namer:
                 raise TypeError("Names must consist of strings, numbers, and number-returning functions")
         if len(nums): name_parts.append(nums)
         self.name_parts = name_parts
-    # TODO: name(data, zero_fill=False)→data2
+    # TODO: name(data, fill=None)→data2
     #   …Which just consists of putting the header in, right?… …Then again, the header is variable due to functions… And should probably be fractal-folded too…
     # TODO: unname(feedback)→feedback2
 
@@ -134,3 +167,8 @@ def _str_to_floats(string: str):
 
 default = Handler()
 # TODO: Also expose method-like funcs that defer to `default`'s stuff.
+
+
+
+# TODO: Also launch tests if this module is executed directly: correctness, then throughput.
+#   (The interface is not hard to use for the "functions that wait for the handler's decision, where some can spawn new functions and such", right? Not quite asyncio levels of simplicity, but still... ...Could we integrate with asyncio too?...)
