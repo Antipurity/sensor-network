aeffcbc1e21124b9818fe1b02ff06c168f23b5bd
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-20 22:58:23 +0200

    py okay, `rnn` is good enough for our purposes now

--- py/model/rnn.py
+++ py/model/rnn.py
@@ -1,14 +1,17 @@
 """
-TODO:
+Contains `RNN`: a simple wrapper for training `fn(state, *args) → state` functions.
+
+Run this file to test it.
 """
 
 
 
 import torch
+import torch.utils.checkpoint
 
 
 
-def RNN(transition, loss, optimizer, backprop_length=64, grad_checkpointing=32, trace=True, async_updates=True):
+def RNN(transition, loss, optimizer, backprop_length=64, checkpoint=True, trace=True):
     """
     Wraps a state→state differentiable dynamical system (commonly a recurrent neural network, RNN) written in PyTorch.
 
@@ -22,25 +25,13 @@ def RNN(transition, loss, optimizer, backprop_length=64, grad_checkpointing=32, 
         - Note that `state` here is post-transition, so for something like next-state prediction, delay `RNN` steps by one so that next-state is always available.
     - `optimizer: torch.optim.Optimizer`: updates the system. Could be wrapped in `lambda p: torch.optim.SGD(p, lr=1e-2)`.
     - `backprop_length = 64`: how many steps to backpropagate gradient through, capped off by `sum(loss).backward()`. Could be wrapped in a function such as `lambda: random.randint(1, 1024)`.
-    - TODO: `grad_checkpointing = 32`: if `None`, no [checkpointing](https://pytorch.org/docs/stable/checkpoint.html): computation is as-quick-as-possible, but used memory grows quickly because all intermediate activations are stored. If an integer `n`, we only store `backprop_length/n` `state`s plus intermediate activations for `n` steps, but do the forward pass twice (so, about 30% slowdown).
-        - TODO: `torch.get_rng_state()→state`, `torch.set_rng_state(state)`
+    - `checkpoint = True`: if `False`, no [checkpointing](https://pytorch.org/docs/stable/checkpoint.html): computation is fast, but used memory grows quickly because all intermediate activations are stored. If `True`, needs less memory, but the forward pass is done twice (so, about 30% slowdown).
     - `trace = True`: if `transition` has no CPU-side control flow, `True` to [precompile](https://pytorch.org/docs/stable/generated/torch.jit.trace.html) for a bit of speed.
-    - TODO: `async_updates=True`: ...Also: if we implement checkpointing manually, then we'd be able to train online by intelligently scheduling backward updates, and copying updates...
-        - WHY CAN WE SUDDENLY THINK OF SO MANY GOOD FEATURES SO LATE
-        - THEY ARE HARD TO IMPLEMENT
-        - WHY NOW
     """
     optimizer = optimizer(transition.parameters()) if callable(optimizer) else optimizer
     assert isinstance(optimizer, torch.optim.Optimizer)
     n, n_max = 0, 0
     total_loss = 0.
-    # TODO: Add checkpointing.
-    #   ...How do we do it...
-    #   TODO: ...Wait: then `loss` has to be computed on the backward pass, not on the forward pass, right?
-    #     ...But then we need to preserve all `*args`... Is this acceptable? I guess it has to be.
-    #     ...But then we can't even implement `n`-dependent `loss` in the test, without implementing a custom module... Uh... That's quite the conundrum.
-    #       How to ignore non-first args, so that we can actually pass in loss info... Okay, now we do.
-    # TODO: Then add async updates (even though we decided to use replay buffers for learning…).
     def new_bptt(state):
         nonlocal total_loss, n, n_max
         # Backprop, and set up for the next backprop.
@@ -63,7 +54,10 @@ def RNN(transition, loss, optimizer, backprop_length=64, grad_checkpointing=32, 
             with torch.no_grad():
                 transition = torch.jit.trace(transition, (state, *args))
                 trace = False
-        state = transition(state, *args)
+        if not checkpoint: # pragma: no cover
+            state = transition(state, *args)
+        else:
+            state = torch.utils.checkpoint.checkpoint(transition, state, *args)
         total_loss += loss(state, *args)
         if n >= n_max: state = new_bptt(state)
         return state
@@ -71,7 +65,7 @@ def RNN(transition, loss, optimizer, backprop_length=64, grad_checkpointing=32, 
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': # pragma: no cover
     """
     A test: next-vector prediction.
 
@@ -88,10 +82,12 @@ if __name__ == '__main__':
     import torch.nn as nn
     import random
     import matplotlib.pyplot as plt
+    import time
+    start = time.monotonic()
     def loss(state, predicts):
         L = (state - predicts).square().sum()
         cL = L.cpu().detach().numpy()
-        print('L2 '+str(iter)+':', cL, '       ', end = '\r' if iter%5000 else '\n')
+        print(''+str(iter), 'L2:', cL, '' if iter%5000 else ('  time: '+str(time.monotonic() - start)+'s'), '       ', end = '\r' if iter%5000 else '\n')
         losses.append(cL)
         return L
     class OnlyFirstArg(nn.Module):
@@ -122,5 +118,3 @@ if __name__ == '__main__':
         n = (n + 1) % data.shape[0]
     plt.plot(losses)
     plt.show()
-# TODO: Rename this file to make it the __init__.py of the `torch_rnn` (or smth) package, and publish it, because, why not. And, async updates are kinda dope.
-#   (But without async updates, pointless to publish this as a separate package.)
