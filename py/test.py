df24ba520933b748644917604174f044354a4b3e
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-23 15:51:27 +0200

    thinkin

--- py/test.py
+++ py/test.py
@@ -15,14 +15,63 @@ from model.momentum_copy import MomentumCopy
 
 
 
+cell_shape, part_size = (8, 24, 64), 8
+sn.shape(cell_shape, part_size)
+
+
+
+class SkipConnection(nn.Module):
+    def __init__(self, *fn): super().__init__();  self.fn = nn.Sequential(*fn)
+    def forward(self, x): return self.fn(x) + x
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# TODO: Create the model...
-#   What parts do we want, exactly?
-#     embed_tokens: input → state
-#     state_transition: state → state
-#     state_future: state → future # BYOL projection.
-#       (With a momentum-copy.)
-#     future_transition: future → future # BYOL predictor.
+hidden_sz = 128
+embed_tokens = nn.Sequential( # input → state
+    nn.Linear(sum(cell_shape), hidden_sz),
+    SkipConnection(
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+        nn.Linear(hidden_sz, hidden_sz),
+    ),
+)
+state_transition = nn.Sequential( # state → state; RNN.
+    # TODO: ...WAIT: we forgot to incorporate `Attention` everywhere...
+    #   Where exactly, and how?...
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+)
+state_future = nn.Sequential( # state → future; BYOL projector.
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+)
+slow_state_future = MomentumCopy(state_future)
+future_transition = nn.Sequential( # future → future; BYOL predictor.
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+    SkipConnection(
+        nn.Linear(hidden_sz, hidden_sz),
+        nn.ReLU(),
+        nn.LayerNorm(hidden_sz),
+    ),
+)
 # TODO: Wrap in RNN...
 #   Do we want a separate class that puts all of the above together, not putting it in the main loop?
 #   Or is all the future-stuff a part of the loss, and the main RNN just `embed_tokens` + `state_transition`?
@@ -31,3 +80,16 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # TODO: An infinite loop where we update state, and display minienv.explored().
 # TODO: Run & test. Try to make it work, at least via trying different normalization schemes.
+
+
+
+# TODO: That "normalization of futures will lead to exploration" seems like a big weak point. So, try:
+# - Cross-cell normalization.
+# - Per-cell normalization (LayerNorm at the end of the future-predictor).
+# - Running statistics (BatchNorm in eval mode).
+# - Very small future-representations.
+#     - Cumulative sum right before normalization?
+#     - Hierarchical downsampling, which may make more sense than cumsum?
+# - Weight decay with long learning?
+# - If all fails, translate the "exploration = max sensitivity of future to the past" definition to code: `state.detach()` for the BYOL-loss, and to train `state`, forward-propagate its gradient to the future, and maximize the sum of its magnitudes. (Hopefully, doesn't explode.) (As a bonus, this returns us to our initial "opposing optimization forces at work" formulation. And doesn't actually contradict the BYOL-paradigm, because the BYOL paper doesn't consider gradient of inputs.)
+# - If ALL fails, switch to SwaV.
