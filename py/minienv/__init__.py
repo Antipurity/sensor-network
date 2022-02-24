e3c4ee90b334f238ccc1f269be4baa776494f78e
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-24 22:14:11 +0200

    fixed world-resetting-when-all-agents-are-dead only happening in the sensor, which isn't called if there are no agents because there are no data&queries (partially fixed)

--- py/minienv/__init__.py
+++ py/minienv/__init__.py
@@ -73,7 +73,7 @@ default_options = {
     'random_connection_probability': .03,
     'avg_resource': .1, # The actual initial resource in a node is 0…avg_resource*2.
     # The top-level-action options.
-    'can_reset_the_world': True, # If checked, each step has a 50% chance of resetting the world, which only adds to exploration.
+    'can_reset_the_world': False, # If checked, each step has a 50% chance of resetting the world, which only adds to exploration. # TODO: Disabled, only for testing.
     # Agents that act on the graph.
     'max_agents': 16, # 1 to disallow forking.
     'allow_suicide': True, # Look at all this proof-of-exploration opportunity.
@@ -127,9 +127,9 @@ def agent(sn, at=..., resource=1., hunger=False):
             reward = 0.
             # Receive actions.
             actions = 4
-            print('agent pre-act') # TODO:
+            print('  agent pre-act', name) # TODO:
             act = await sn.query(name=(name, at, 'act'), query = options['node_name_size'] + actions)
-            print('agent post-act') # TODO:
+            print('  agent post-act', name) # TODO:
             if act is None: continue # Re-send observations on dropped packets.
             data, acts = act[:-actions], act[-actions:]
             # Bleed onto a random node. Die if unfortunate.
@@ -138,7 +138,7 @@ def agent(sn, at=..., resource=1., hunger=False):
             nodes[random.choice(nodes['all'])][2] += dresource
             at_resource = nodes[at][2]
             if resource <= 0:
-                print('agent DIED') # TODO:
+                print('agent DIED', name) # TODO:
                 del agents[name]
                 break
             agents[name][2] = resource
@@ -157,17 +157,22 @@ def agent(sn, at=..., resource=1., hunger=False):
                 print('    agent forked at', at) # TODO:
             # Goto neighbor.
             if acts[2]>0 and len(neighbors):
-                nearest_neighbor_i = np.argmin(np.sum(np.abs(data - np.concatenate([nodes[ng][1] for ng in neighbors], 0)))) # TODO: ...WAIT: why is it complaining about this line ("axis 1 is out of bounds for array of dimension 1") when we keyboard-interrupt?... DAMN YOU, PYTHON
+                nearest_neighbor_i = np.argmin(np.sum(np.abs(data - np.concatenate([nodes[ng][1] for ng in neighbors], 0)))) # TODO: ...Why is it now complaining that "operands could not be broadcast together with shapes (16,) (32,)"...
                 at = neighbors[nearest_neighbor_i]
                 agents[name][1]
                 print('    agent nearest neighbor is', nearest_neighbor_i, at) # TODO:
             # Un-fork.
             if acts[3]>0 and options['allow_suicide']:
                 print('    agent', name, 'UNFORKED, now have', len(agents.keys())-1) # TODO: ...Why do we seemingly continue after unforking a last agent? ...Or after another action... Without the 'agent EXITED' notification, and without ever getting past the "agent pre-act" phase (so, maybe futures are getting swallowed or something?)...
+                #   TODO: ...But we've also seen post-act being OK, but nothing afterwards...
+                #   TODO: ...We've also seen stalling after successfully unforking the last agent. What? ...Maybe, the handler expects data, but we only provide data in the sensor, which no longer even provides data apart from creating agents...
                 nodes[at][2] += resource
                 del agents[name]
                 break
-        print('agent EXITED', name) # TODO:
+        print('agent EXITED', name, 'resetting:', not agents) # TODO:
+        if not agents:
+            reset(**options)
+            agent(sn) # TODO: WHY DID THIS NOT SOLVE THIS COMPLETELY
     agents[name] = [asyncio.ensure_future(loop()), at, resource, hunger]
     return name
 
@@ -228,13 +233,11 @@ def _top_level_actions(sn):
     if options['can_reset_the_world']:
         sn.query(name=('world', 'reset'), query=sn.cell_shape[-1], callback=_maybe_reset_the_world)
     if not options['stop']:
-        print(' agents', len(agents.keys())) # TODO: Does this happen when we have no-action continuing... ...Wait, why is the 'unforked' agent of a different name than remaining agents? It was never even *in* `agents`; the heck?
+        print(' agents', list(agents.keys())) # TODO: Does this happen when we have no-action continuing... ...Wait, why is the 'unforked' agent of a different name than remaining agents? It was never even *in* `agents`; the heck?
         if not agents:
             if options['please_make_an_agent']:
                 agent(sn)
                 options['please_make_an_agent'] = False
-            else: # Everyone. Dead.
-                reset()
 
 
 
