2c57af2279cb9f1415adfcfe69a3e41f0f38dae2
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-02-28 22:38:52 +0200

    at least wrote down the decorrelation strength

--- py/model/loss.py
+++ py/model/loss.py
@@ -13,35 +13,41 @@ import torch.nn as nn
 
 class CrossCorrelationLoss(nn.Module):
     """
-    `CrossCorrelationLoss(axis=0)(x, y)`
+    `CrossCorrelationLoss(axis=0, decorrelation_strength=1.)(x, y)`
 
-    Basically `(x.norm(axis) @ y.norm(axis).t() - eye(x.shape[axis])).square().mean(axis).sum()`.
+    By default, `(x.norm(-2) @ y.norm(-2).t() - eye(x.shape[-2])).square().mean(-2).sum()`.
 
     A differentiable dissimilarity metric that makes [normalized cross-](https://github.com/rogerberm/pytorch-ncc)[correlation](https://en.wikipedia.org/wiki/Cross-correlation) (or pairwise [cosine similarity](https://en.wikipedia.org/wiki/Cosine_similarity) if done on matrices) as close to the identity matrix as posssible when minimized (`.backward()`).
 
     Why: non-pairwise losses can't enforce the lack of redundancy, which makes predicting your own output just turn it into a constant. But this loss does enforce that, [making it usable for self-distillation.](https://arxiv.org/abs/2103.03230)
 
     Parameters:
-    - `axis = 0`: TODO:
+    - `axis = 0`: the dimension to compute similarity over. Inputs are put through `x → x.transpose(axis, -2)` before the matrix-multiplication.
+    - `decorrelation_strength = 1`: the loss target is separated into `1`s and `0`s, and the loss to `0`s is multiplied by this. The closer to `0`, the more important actual prediction is.
     - TODO:
     """
-    def __init__(self, axis=0): # TODO: What args do we want?
-        # TODO: The correlation-removal-strength coefficient.
-        # TODO: The shuffle-invariant mode (which makes targets 1s wherever they are the max on both axes).
+    def __init__(self, axis=0, decorrelation_strength = 1.):
         # TODO: A way to choose between L1 and L2 loss.
+        #   Do we want just 'l1' and 'l2', or a function? Or either?
+        # TODO: The shuffle-invariant mode (which makes targets 1s wherever they are the max on both axes).
         super().__init__()
         self.axis = axis
+        self.decorrelation_strength = decorrelation_strength
     def forward(self, x, y):
         assert x.shape == y.shape
         while len(x.shape) < 2: x = x.unsqueeze(0);  y = y.unsqueeze(0)
         x = x.transpose(self.axis, -2);  y = y.transpose(self.axis, -2)
         x = (x - x.mean(-2)) / (x.std(-2) + 1e-5)
         y = (y - y.mean(-2)) / (y.std(-2) + 1e-5)
-        cc = torch.matmul(x, y.t())
-        return (cc - torch.eye(cc.shape[-2], cc.shape[-1])).square().mean(-2).sum()
+        cc = torch.matmul(x, y.transpose(-2, -1))
+        target = torch.eye(cc.shape[-2], cc.shape[-1], device = x.device)
+        part1 = target * (cc - 1).square()
+        part2 = self.decorrelation_strength * (1 - target) * cc.square()
+        return (part1 + part2).mean(-2).sum()
 
 
 
 if __name__ == '__main__':
     pass
     # TODO: Tests of all parameter configurations, especially that a shuffle-invariant loss always converges to a shuffled identity matrix.
+    # TODO: Test decorrelation_strength.
