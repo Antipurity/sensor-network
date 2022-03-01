2aa5d62534d60bcc83e1310ac070a42f6d9b62e6
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-03-01 04:15:59 +0200

    removed some bias in the loss, only relevant for very-small vectors
    shuffle-invariance is broken
    
    could not fix for now

--- py/model/loss.py
+++ py/model/loss.py
@@ -11,6 +11,9 @@ import torch.nn as nn
 
 
 
+import random # TODO:
+import matplotlib.pyplot as plt # TODO:
+IM = None # TODO:
 class CrossCorrelationLoss(nn.Module):
     """
     `CrossCorrelationLoss(axis = -1, decorrelation_strength = 1., prediction = 'l2', shuffle_invariant = False)(x, y)`
@@ -21,7 +24,7 @@ class CrossCorrelationLoss(nn.Module):
 
     Why: non-pairwise losses can't enforce the lack of redundancy, which makes predicting your own output just turn it into a constant. But this loss does enforce that, [making it usable for self-distillation.](https://arxiv.org/abs/2103.03230)
 
-    If printing the loss, be advised that it is frequently stuck at `.97` or similar, but learning still happens.
+    If printing the loss, be advised, it is frequently almost-stuck at `.9786` or similar, but learning still happens.
 
     Parameters:
     - `axis = -1`: the dimension to compute similarity over. Inputs are put through `x → x.transpose(axis, -2)` before the matrix-multiplication.
@@ -29,8 +32,11 @@ class CrossCorrelationLoss(nn.Module):
     - `prediction = 'l2'`: [either `'l1'` or `'l2'`.](https://machine-learning-note.readthedocs.io/en/latest/basic/loss_functions.html)
     - `shuffle_invariant = False`: if `True`, the target identity matrix (cross-correlation of a sequence with itself) is replaced with its closest shuffle. Useful if the inputs were computed by a Transformer.
 
-    Examples:
+    Notes:
     - `decorrelation_strength > 0` and `prediction = 'l1'`: learning hardly happens, because gradients self-interfere too much.
+    - Performs best when both `x` and `y` are differentiable.
+    - The more data, the better it learns (L2 loss of normalized vectors can get lower).
+    - Optimizing with Adam seems to perform much better than with SGD or RMSprop.
     """
     def __init__(self, axis = -1, decorrelation_strength = .1, prediction = 'l2', shuffle_invariant = False):
         assert prediction == 'l1' or prediction == 'l2'
@@ -47,41 +53,56 @@ class CrossCorrelationLoss(nn.Module):
         while len(x.shape) < 2:
             x = x.unsqueeze(0);  y = y.unsqueeze(0);  axis += 1
         x = x.transpose(axis, -2);  y = y.transpose(axis, -2)
-        x = (x - x.mean(-2, True)) / (x.std(-2, keepdim=True) + 1e-5)
-        y = (y - y.mean(-2, True)) / (y.std(-2, keepdim=True) + 1e-5)
+        eps = 1e-5
+        x = (x - x.mean(-2, True)) / (x.std(-2, keepdim=True) + eps)
+        y = (y - y.mean(-2, True)) / (y.std(-2, keepdim=True) + eps)
         cc = torch.matmul(x, y.transpose(-2, -1)) / x.shape[-2]
         if not self.shuffle_invariant:
-            target = torch.eye(cc.shape[-2], cc.shape[-1], device = x.device)
+            mask = torch.eye(cc.shape[-2], cc.shape[-1], device = x.device)
         else:
             A1 = (cc - cc.max(-2, True)[0]).sign()+1.
+            # TODO: ...Should we modify cc to not contain entries from A1?... Would that really ensure orderliness though?...
             A2 = (cc - cc.max(-1, True)[0]).sign()+1.
-            target = torch.maximum(A1, A2)
-        # TODO: Should try printing those `cc` matrices, because 2D isn't learning well. Or at least, the non-zero loss is annoying to look at.
-        #   Do we want matplotlib? Can it update dynamically?
-        part1 = target * ((cc - 1).square() if self.l2 else (cc - 1).abs())
-        part2 = self.decorrelation_strength * (1 - target) * (cc.square() if self.l2 else cc.abs())
+            # TODO: Why is this broken, even just the separate A1/A2? Can we make it not-broken?
+            #   (After all, Transformers are unordered, so can we really just impose ordered CCL on their outputs?)
+            mask = A1 # (A1 + A2) / 2 # torch.maximum(A1, A2)
+        target = (mask - mask.mean())
+        if random.randint(1,1) == 1: # TODO: Remove this, after we've debugged shuffle.
+            global IM
+            if IM is None:
+                IM = plt.imshow(mask.detach().cpu().numpy()) # TODO:
+            else:
+                IM.set_data(mask.detach().cpu().numpy()) # TODO: Does this mask just converge to this, or is our computation broken? Looks like extremely-fast convergence...
+            plt.pause(.01)
+        cc = cc - target
+        part1 = mask * (cc.square() if self.l2 else cc.abs())
+        part2 = self.decorrelation_strength * (1 - mask) * (cc.square() if self.l2 else cc.abs())
         return (part1 + part2).mean(-2).sum()
 
 
 
 if __name__ == '__main__': # Tests.
     from torch import randn
-    from torch.optim import SGD
+    from torch.optim import Adam
     CCL = CrossCorrelationLoss
-    # Testing 1 dim.
-    input, fn, output, loss, losses = randn(3, 128), nn.Sequential(
-        nn.Linear(128, 256),
+
+    cells, out_sz = 16, 128
+    input, fn, fn2, output, loss, losses = randn(cells, out_sz), nn.Sequential(
+        nn.Linear(out_sz, 128),
         nn.ReLU(),
-        nn.Linear(256, 96),
-    ), randn(3, 96), CCL(axis=-1), []
-    opt = SGD(fn.parameters(), 1e-2)
-    for _ in range(5000):
-        A, B = fn(input), output
+        nn.Linear(128, out_sz),
+    ), nn.Sequential(
+        nn.Linear(out_sz, 128),
+        nn.ReLU(),
+        nn.Linear(128, out_sz),
+    ), randn(cells, out_sz), CCL(axis=-1, decorrelation_strength=.1, shuffle_invariant=True), [] # TODO: .1
+    opt = Adam([*fn.parameters(), *fn2.parameters()], 1e-4)
+    for _ in range(50000): # TODO: 5000
+        A, B = fn(input), fn2(output)
         L = loss(A, B)
         A = (A - A.mean(-1, True)) / (A.std(-1, keepdim=True) + 1e-5)
         B = (B - B.mean(-1, True)) / (B.std(-1, keepdim=True) + 1e-5)
-        print('norm CCL', str(L.detach().cpu().numpy()).ljust(10), '    norm L2', (A-B).square().sum().detach().cpu().numpy())
+        print('norm CCL', str(L.detach().cpu().numpy()).ljust(11), '    norm L2', (A-B).square().sum().detach().cpu().numpy())
         L.backward();  opt.step();  opt.zero_grad()
-        # TODO: ...Why is an unchanging CCL loss still decrease L2 loss? And more importantly, why is it .9895 for most of optimization, and why doesn't it tend to 0?
-        #   Cross-correlation is not 1 because of our normalization, which makes the square of some entries just never equal to 1. Still annoying, though.
-    # TODO: Test that shuffle-invariance always converges to a `target` matrix with .sum() being x.shape[-2].
+
+    # TODO: Test that shuffle-invariance always converges to a `target` matrix with .sum() being x.shape[-2]. (& fix.)
