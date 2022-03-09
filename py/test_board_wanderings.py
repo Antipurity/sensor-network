e7511302c44a04c3d474ad9f15b3bbb3065e58b0
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-03-09 16:06:46 +0200

    (there sure are bugs, but where...)

--- py/test_board_wanderings.py
+++ py/test_board_wanderings.py
@@ -8,6 +8,10 @@ NO RL machinery! Lessons must be transferrable all the way.
 
 
 
+from model.momentum_copy import MomentumCopy
+
+
+
 import torch
 import torch.nn as nn
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -45,11 +49,13 @@ class SkipConnection(nn.Module):
 
 
 
-N, batch_size = 8, 1000
-state_sz = 64
-overparameterized = 4
+# (TODO: This only worked once, with super-simple hyperparams, can't reproduce, so should try a GAN.)
+N, batch_size = 2, 100
+state_sz = 2
+overparameterized = 40
 
-unroll_len, denoising_levels = N*2, 3
+unroll_len, denoising_levels = 1, 1
+# unroll_len, denoising_levels = 2*N-2, 2
 
 
 
@@ -57,49 +63,75 @@ next = nn.Sequential( # (board, target_board, state, output) → output_state
     # (`output` is for sampling from the action distribution on each step, initially a random vector in each iteration, denoised one or more times.)
     # (The actions are simply sliced from `output_state`.)
     nn.Linear(N*N + N*N + state_sz + state_sz, overparameterized * state_sz),
-    SkipConnection(nn.Sequential(
-        nn.LayerNorm(overparameterized * state_sz),
-        nn.ReLU(),
-        nn.Linear(overparameterized * state_sz, overparameterized * state_sz),
-    )),
-    SkipConnection(nn.Sequential(
-        nn.LayerNorm(overparameterized * state_sz),
-        nn.ReLU(),
-        nn.Linear(overparameterized * state_sz, overparameterized * state_sz),
-    )),
+    # SkipConnection(nn.Sequential(
+    #     nn.LayerNorm(overparameterized * state_sz),
+    #     nn.ReLU(),
+    #     nn.Linear(overparameterized * state_sz, overparameterized * state_sz),
+    # )),
     nn.LayerNorm(overparameterized * state_sz),
     nn.ReLU(),
     nn.Linear(overparameterized * state_sz, state_sz),
 ).to(device)
-opt = torch.optim.Adam(next.parameters(), lr=1e-4)
+next_discriminator = nn.Sequential( # (board, target_board, state, output) → will_reach_target
+    nn.Linear(N*N + N*N + state_sz + state_sz, overparameterized * state_sz),
+    # SkipConnection(nn.Sequential(
+    #     nn.LayerNorm(overparameterized * state_sz),
+    #     nn.ReLU(),
+    #     nn.Linear(overparameterized * state_sz, overparameterized * state_sz),
+    # )),
+    nn.LayerNorm(overparameterized * state_sz),
+    nn.ReLU(),
+    nn.Linear(overparameterized * state_sz, 1, bias=False),
+    nn.Softsign(),
+).to(device)
+next_discriminator_copy = MomentumCopy(next_discriminator, .99)
+opt = torch.optim.Adam(next.parameters(), lr=1e-3)
 for iters in range(50000):
     # TODO: Run & fix. (Maybe not as good as a GAN, but maybe we can actually get the correct-target-percentage to go up.)
     # Sample a batch of trajectories (pre-deciding the target-board), accumulating the denoising loss, and minimizing it wherever we've reached the target.
     L2 = 0
-    state = torch.zeros(batch_size, state_sz, device=device) # For now, only use the max-denoised state.
+    state = torch.zeros(batch_size, state_sz, device=device)
     board = env_init(N, batch_size=batch_size)
-    target_board = env_init(N, batch_size=batch_size)
+    target_board = env_init(N, batch_size=batch_size) # TODO: ...What if there's only ever one target... Or, only ever one initial-state?... (WHERE DID WE GO WRONG)
     achieved_target = torch.full((batch_size,), False, device=device)
+    target_reachable = []
     for u in range(unroll_len):
         # Do the RNN transition (and an environment step), `unroll_len` times.
         nexts = [torch.randn(batch_size, state_sz, device=device)]
-        for _ in range(denoising_levels):
+        for lvl in range(denoising_levels):
             # Denoise the next-state, `denoising_levels` times.
             #   (Diffusion models learn to reverse gradual noising of samples, with thousands of denoising steps. That's too slow in an RNN, so we learn the reversing directly.)
-            #   (…Would this just learn the fixed-points and then stop producing any diversity?… I think it would, since the only randomness is at the beginning, which we learn to ignore by design… Would diffusion models fare better…? Probably; but would a GAN? I think a GAN is pretty much the same, honestly, just with a learned target (changes in which might actually save us).)
+            #   (Doesn't seem to work, though. Maybe a GAN would have better luck.)
             nexts.append(next(torch.cat((board, target_board, state, nexts[-1]), -1)))
-        for noised in nexts[1:-1]:
+            input = torch.cat((board, target_board, state, nexts[-1]), -1)
+            target_reachable.append(next_discriminator(input.detach()).sum(-1))
+            print(next_discriminator(input.detach()).mean().detach().cpu().numpy()) # TODO: ...Wait, why is it all negative? Quite suspicious, honestly...
+            #   TODO: ...And why is this mean going toward `1` when the correct-target percentage is 25% and such?!
+            # L2 = L2 + (next_discriminator_copy(input) - 1).square().sum() # TODO: ...Why is even a GAN not working... In fact, how are we even reaching 20K in discriminator loss?... WHAT DID WE IMPLEMENT WRONG?!
+            #   ...It doesn't make any sense for the model to be completely unable to learn whether single steps will lead to success, right? SO WHERE ARE THE BUGS
+        for lvl, noised in enumerate(nexts[1:-1]):
             # (Preserve the batch dimension, so that we could select which to minimize.)
             L2 = L2 + (noised - nexts[-1]).square().sum(-1)
         state = nexts[-1]
         board = env_step(N, board, state[..., 0:2])
         achieved_target = achieved_target | (board == target_board).all(-1)
-    L2 = (L2 * achieved_target.float()).sum()
-    L2.backward()
-    opt.step();  opt.zero_grad(True)
+    achieved_target = achieved_target.float()
+    L2 = (L2 * achieved_target).sum()
+    L22 = 0
+    for reachable in target_reachable:
+        print('discriminator guessed correctly', str((((reachable > 0).float() == achieved_target).float().mean()*100).detach().cpu().numpy())+'%') # TODO: ...How can it possibly be such a high percentage from the very start?! ...Probably all-OK or all-non-OK, since percentages match. ...But why is that even so...
+        #   Why is nearly every run initialized to be discriminated as an extreme?! Does `Linear` add a bias that's too big for us or something?... Should we use a non-linearity at the end?...
+        L22 = L22 + (reachable - achieved_target*2-1).square().sum() # TODO: ...Why can't we learn this, even still??
+    (L2 + L22).backward()
+    opt.step();  opt.zero_grad(True);  next_discriminator_copy.update()
     with torch.no_grad():
-        correct_frac = achieved_target.float().sum() / batch_size
-        print(str(iters).rjust(6), 'L2', str(L2.detach().cpu().numpy()).ljust(11), 'correct target', str((correct_frac*100).cpu().numpy())+'%')
+        correct_frac = achieved_target.mean()
+        print(
+            str(iters).rjust(6),
+            'denoising L2', str(L2.detach().cpu().numpy()).ljust(13),
+            'discriminator L2', str(L22.detach().cpu().numpy()).ljust(13),
+            'correct target', str((correct_frac*100).cpu().numpy())+'%',
+        )
 # TODO: Okay, what do we want to learn, building up to URL gradually?
 #   - ✓ From board and action (randomly-generated) to board — EASY
 #   - ✓ From board and neighboring-board (gotten via env_step with a random action) to action (the randomly-generated one) — will be so easy that it's pointless to implement.
