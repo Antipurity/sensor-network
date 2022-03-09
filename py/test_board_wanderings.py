93c165532aeed467394a551899d225e9f5af1132
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-03-09 21:36:16 +0200

    GANs seem too unstable to be usable so far; may just be a bad implementation though

--- py/test_board_wanderings.py
+++ py/test_board_wanderings.py
@@ -9,6 +9,7 @@ NO RL machinery! Lessons must be transferrable all the way.
 
 
 from model.momentum_copy import MomentumCopy
+from model.log import log
 
 
 
@@ -50,12 +51,12 @@ class SkipConnection(nn.Module):
 
 
 # (TODO: This only worked once, with super-simple hyperparams, can't reproduce, so should try a GAN.)
-N, batch_size = 2, 100
-state_sz = 2
-overparameterized = 40
+N, batch_size = 8, 100
+state_sz = 16
+overparameterized = 8
 
-unroll_len, denoising_levels = 1, 1
-# unroll_len, denoising_levels = 2*N-2, 2
+# unroll_len, denoising_levels = 1, 1
+unroll_len, denoising_levels = 2*N-2, 1
 
 
 
@@ -84,35 +85,30 @@ next_discriminator = nn.Sequential( # (board, target_board, state, output) → w
     nn.Linear(overparameterized * state_sz, 1, bias=False),
     nn.Softsign(),
 ).to(device)
-next_discriminator_copy = MomentumCopy(next_discriminator, .99)
-opt = torch.optim.Adam([*next.parameters(), *next_discriminator.parameters()], lr=1e-3)
-frozen_state = torch.randn(batch_size, state_sz, device=device) # TODO: ...This is freaky: why can we not learn to be better than random chance even with frozen generator state?!
-target_board = env_init(N, batch_size=batch_size) # TODO: ...What if there's only ever one target... Or, only ever one initial-state?... (WHERE DID WE GO WRONG)
+next_discriminator_copy = MomentumCopy(next_discriminator, .999)
+opt = torch.optim.Adam([*next.parameters(), *next_discriminator.parameters()], lr=1e-4)
 for iters in range(50000):
-    # TODO: Run & fix. (Maybe not as good as a GAN, but maybe we can actually get the correct-target-percentage to go up.)
+    # TODO: Run & fix. (Maybe not as good as a GAN, but maybe we can actually get the correct-target-percentage to go up. …We couldn't, in the end.)
     # Sample a batch of trajectories (pre-deciding the target-board), accumulating the denoising loss, and minimizing it wherever we've reached the target.
     L2 = 0
     state = torch.zeros(batch_size, state_sz, device=device)
     board = env_init(N, batch_size=batch_size)
-    # target_board = env_init(N, batch_size=batch_size) # TODO: ...What if there's only ever one target... Or, only ever one initial-state?... (WHERE DID WE GO WRONG)
+    target_board = env_init(N, batch_size=batch_size)
     achieved_target = torch.full((batch_size,), False, device=device)
     target_reachable = []
     for u in range(unroll_len):
         # Do the RNN transition (and an environment step), `unroll_len` times.
-        nexts = [frozen_state]
-        # nexts = [torch.randn(batch_size, state_sz, device=device)] # TODO:
+        nexts = [torch.randn(batch_size, state_sz, device=device)]
         for lvl in range(denoising_levels):
             # Denoise the next-state, `denoising_levels` times.
             #   (Diffusion models learn to reverse gradual noising of samples, with thousands of denoising steps. That's too slow in an RNN, so we learn the reversing directly.)
             #   (Doesn't seem to work, though. Maybe a GAN would have better luck.)
             nexts.append(next(torch.cat((board, target_board, state, nexts[-1]), -1)))
             input = torch.cat((board, target_board, state, nexts[-1]), -1)
-            # print(input[0].detach().cpu().numpy()) # TODO:
             target_reachable.append(next_discriminator(input.detach()).sum(-1))
-            print(target_reachable[-1].mean().detach().cpu().numpy()) # TODO: ...Wait, why is it all negative? Quite suspicious, honestly...
-            #   TODO: ...And why is this mean going toward `1` when the correct-target percentage is 25% and such?!
-            # L2 = L2 + (next_discriminator_copy(input) - 1).square().sum() # TODO: ...Why is even a GAN not working... In fact, how are we even reaching 20K in discriminator loss?... WHAT DID WE IMPLEMENT WRONG?!
-            #   ...It doesn't make any sense for the model to be completely unable to learn whether single steps will lead to success, right? SO WHERE ARE THE BUGS
+            L2 = L2 + (next_discriminator_copy(input) - 1).square().sum() # TODO: …Why doesn't this seem to help make more targets correct?…
+            #   …Wait, suddenly it did… Once; the rest of the time, goes up to 75% intermittently, and is unstable…
+            #   TODO: ...So wait a second: why is generator loss so much higher than discriminator loss (100K vs 1K)? Seems suspicious...
         for lvl, noised in enumerate(nexts[1:-1]):
             # (Preserve the batch dimension, so that we could select which to minimize.)
             L2 = L2 + (noised - nexts[-1]).square().sum(-1)
@@ -123,19 +119,20 @@ for iters in range(50000):
     L2 = (L2 * achieved_target).sum()
     L22 = 0
     for reachable in target_reachable:
-        print('                                                                 discriminator guessed correctly', str((((reachable > 0).float() == achieved_target).float().mean()*100).round().detach().cpu().numpy())+'%', '>0', str(((reachable > 0).float().mean()*100).round().detach().cpu().numpy())+'%') # TODO: ...How can it possibly be such a high percentage from the very start?! ...Probably all-OK or all-non-OK, since percentages match. ...But why is that even so...
-        #   Why is nearly every run initialized to be discriminated as an extreme?! AND WHY CAN'T IT EVEN CONVERGE ON THIS TRIVIAL TASK, OF FITTING 4 FIXED INPUTS TO 4 FIXED OUTPUTS?!
-        L22 = L22 + (reachable - (achieved_target*2-1)).square().sum() # TODO: ...Why can't we learn this, even still??
-        #   TODO: ...Wait, something is still very wrong: why is the mean very decidedly going up, when `achieved_targets.mean()` is very clearly ~25%?! ...And why is putting `-` instead of `+` here not repel `reachable` from 0 but instead always make it negative even if it was positive at the beginning...
+        # print('                                                                 discriminator guessed correctly', str((((reachable > 0).float() == achieved_target).float().mean()*100).round().detach().cpu().numpy())+'%', '>0', str(((reachable > 0).float().mean()*100).round().detach().cpu().numpy())+'%') # TODO:
+        L22 = L22 + (reachable - (achieved_target*2-1)).square().sum()
     (L2 + L22).backward()
     opt.step();  opt.zero_grad(True);  next_discriminator_copy.update()
     with torch.no_grad():
         correct_frac = achieved_target.mean()
+        log(0, False, generator_L2 = L2.detach().cpu().numpy())
+        log(1, False, discriminator_L2 = L22.detach().cpu().numpy())
+        log(2, False, correct_target_perc = (correct_frac*100).round().cpu().numpy())
         print(
             str(iters).rjust(6),
-            'denoising L2', str(L2.detach().cpu().numpy()).ljust(13),
-            'discriminator L2', str(L22.detach().cpu().numpy()).ljust(13),
-            'correct target', str((correct_frac*100).round().cpu().numpy())+'%',
+            # 'denoising L2', str(L2.detach().cpu().numpy()).ljust(13),
+            # 'discriminator L2', str(L22.detach().cpu().numpy()).ljust(13),
+            # 'correct target', str((correct_frac*100).round().cpu().numpy())+'%',
         )
 # TODO: Okay, what do we want to learn, building up to URL gradually?
 #   - ✓ From board and action (randomly-generated) to board — EASY
