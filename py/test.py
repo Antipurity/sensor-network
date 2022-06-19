17a7ea3858e9503469353fa555ee710079893c49
Antipurity <45312367+Antipurity@users.noreply.github.com>
2022-06-19 22:50:44 +0300

    oversightfix
    (…now trying to use forward AD with index that does not support it…)

--- py/test.py
+++ py/test.py
@@ -422,7 +422,7 @@ def sample_goal(cells, goal_group, analog_prob=.5):
 # Loss.
 def cell_dropout(frame: torch.Tensor, frame_names: np.ndarray):
     """Makes querying subsets of cells not out-of-distribution, by dropping & shuffling cells randomly at training-time. Call & destructure before `per_goal_loss`."""
-    inds = np.choice(np.arange(0, frame.shape[0]), random.randint(1, frame.shape[0]), replace=False)
+    inds = np.random.choice(np.arange(0, frame.shape[0]), random.randint(1, frame.shape[0]), replace=False)
     return frame[inds], frame_names[inds]
 def augment(frame: torch.Tensor) -> torch.Tensor:
     """Augments a frame, so that VAEs can be trained to minimize both dependence on input and cross-input dependence, and so that we can sample even without knowing queries (for imagined trajectories).
